e46ed0a8cd7163dc2050f6bce2848441634eabf8
Notebook-Naza <horaciofalleti@gmail.com>
2022-01-03 22:58:39 -0300

    third commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,14 +28,16 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  var string = (str);
+  return string;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var sumar = x + y ;
+  return sumar;
 }
 
 function resta(x, y) {

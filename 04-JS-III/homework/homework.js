1cd0a229459d9d8ee97619e20e17530c9acd16ae
Notebook-Naza <horaciofalleti@gmail.com>
2022-01-08 10:28:27 -0300

    second commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -116,13 +116,23 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  producto = 1;
+  if (arguments.length === 0 ) {return 0;}
+  if (arguments.length === 1 ) {return arguments[0];}
+  for ( i = 0 ; i < arguments.length ; i++ )
+    producto = producto * arguments[i];
+  return producto;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyos valores sean mayor a 18.
   //Escribe tu código aquí
-
+  cantidad = 0;
+  for ( i = 0 ; i < arreglo.length ; i++ )
+    { if ( arreglo[i] > 18 )
+      cantidad++}
+  return cantidad;
 }
 
 
@@ -131,7 +141,14 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  switch (numeroDeDia){
+    case 1:
+    case 7:
+      return "Es fin de semana";
+      break;
+    default:
+      return "Es dia Laboral";
+  }
 } 
 
 
@@ -139,7 +156,9 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var cadena = String(n);
+  if (cadena[0]==="9") {return true;}
+  return false;
 }
 
 
@@ -147,7 +166,10 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  igual = arreglo[0];
+  for (i = 1 ; i < arreglo.length ; i++)
+    if (arreglo[i] !== igual) {return false;}
+  return true;
 } 
 
 
@@ -156,13 +178,28 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-}
+  var j = 0;
+  var arreglo = [];
+  for ( i = 0 ; i < array.length ; i++ )
+    if ( array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre" ) {
+      arreglo[j] = array[i];
+      j++;}
+  if ( arreglo.length === 3) {return arreglo;}
+    return "No se encontraron los meses pedidos";      
+  }
 
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var j = 0;
+  var mayor = [];
+  for ( i = 0 ; i< array.length ; i++ )
+    if (array[i] > 100 ){
+      mayor[j] = array[i];
+      j++;}
+  return mayor;
 }
 
 
@@ -174,9 +211,15 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var array = [];
+  suma = numero;
+  for ( i = 0 ; i < 10 ; i++) { 
+    suma = suma + 2;
+    if ( suma === i ) break;
+      else array.push(suma);}
+  if ( i < 9 ) {return "Se interrumpió la ejecución";}
+    else return array;
 }
-
-
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
@@ -184,8 +227,15 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-}
+  var array = [];
+  suma = numero;
+  for ( i = 0 ; i < 10 ; i++) {
+    if ( i === 5 ) continue;      
+      else suma = suma + 2;
+           array.push(suma);}
+  return array;
 
+}
 
 // No modificar nada debajo de esta línea
 // --------------------------------
